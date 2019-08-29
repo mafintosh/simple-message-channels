4506be70f9adeb0273dedcb03198a73728097875
Mathias Buus <mathiasbuus@gmail.com>
2019-08-29 14:50:03 +0200

    support empty length messages

--- index.js
+++ index.js
@@ -69,6 +69,7 @@ module.exports = class SimpleMessageChannels {
         this._factor = 1
         this._length = this._varint
         this._consumed = this._varint = 0
+        if (this._length === 0) this._state = 0
         return true
 
       case 1:
