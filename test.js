2d42205c1cc162502f1a4562f9124de2808df74c
Mathias Buus <mathiasbuus@gmail.com>
2019-10-08 14:27:51 +0200

    add failing chunking test

--- test.js
+++ test.js
@@ -61,6 +61,37 @@ tape('two messages chunked', function (t) {
   }
 })
 
+tape('two big messages chunked', function (t) {
+  t.plan(6)
+
+  const expected = [
+    [0, 1, Buffer.alloc(1e5)],
+    [42, 3, Buffer.alloc(2e5)]
+  ]
+
+  const a = new SMC({
+    onmessage (channel, type, message) {
+      const e = expected.shift()
+      t.same(channel, e[0])
+      t.same(type, e[1])
+      t.same(message, e[2])
+    }
+  })
+
+  const payload = a.send(0, 1, Buffer.alloc(1e5))
+
+  for (let i = 0; i < payload.length; i += 500) {
+    a.recv(payload.slice(i, i + 500))
+  }
+
+  const payload2 = a.send(42, 3, Buffer.alloc(2e5))
+
+  for (let i = 0; i < payload2.length; i += 500) {
+    a.recv(payload2.slice(i, i + 500))
+  }
+})
+
+
 tape('empty message', function (t) {
   const a = new SMC({
     onmessage (channel, type, message) {
